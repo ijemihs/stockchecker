219d3e26423db56a7afeaada726d9f246260c15f
shirokuma <my_determination_210292@yahoo.co.jp>
2021-02-03 08:09:36 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>ユーザ登録</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'signup.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', '名前') !!}
+                    {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password_confirmation', 'パスワード(確認用)') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    <p>※パスワードは8文字以上で入力してください。</p>
+                </div>
+
+                {!! Form::submit('登録', ['class' => 'btn btn-brown btn-block']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
