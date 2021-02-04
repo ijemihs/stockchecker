784263ef1557bdce82a6c6e713b2433edc8753e9
shirokuma <my_determination_210292@yahoo.co.jp>
2021-02-04 12:52:05 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,10 +10,29 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                @if (Auth::check())
+                    {{-- 商品検索ページへのリンク --}}
+                    <li class="nav-item"><a href="#" class="nav-link">商品検索</a></li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- ユーザ情報ページへのリンク --}}
+                            <li class="dropdown-item"><a href="#">ユーザ情報</a></li>
+                            {{-- パスワード変更ページへのリンク --}}
+                            <li class="dropdown-item"><a href="#">パスワード変更</a></li>
+                            {{-- お気に入り一覧ページへのリンク --}}
+                            <li class="dropdown-item"><a href="#">お気に入り一覧</a></li>
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                        </ul>
+                    </li>
+                @else
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
+    @if (Auth::check())
         <div class="text-center">
-            <h1>StockCheckerトップページ</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'ご利用開始はこちら', [], ['class' => 'btn btn-lg btn-brown']) !!}
+            <h1>お気に入り一覧</h1>
         </div>
-    </div>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>StockCheckerトップページ</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'ご利用開始はこちら', [], ['class' => 'btn btn-lg btn-brown']) !!}
+            </div>
+        </div>
+    @endif
 @endsection
